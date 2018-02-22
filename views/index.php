17a3d9a23b7d3d3ab78189c248048f3e283fddbc
dalas98 <dalas98@gmail.com>
2018-02-22 11:01:35 +0700

    test

--- views/index.php
+++ views/index.php
@@ -1,3 +1,14 @@
-<?php
-echo 'Hello WOrld';
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php
+		echo "HEllow";
+	?>
+</body>
+</html>
